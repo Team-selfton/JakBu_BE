6e725caebcbd2c115af0ba73b7431e38a64365e3
박태수 <taesupark0504@gmail.com>
2025-12-08 11:06:57 +0900

    chore :: set

---
+++ src/main/java/com/example/jakbu_be/JakBuBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jakbu_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakBuBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JakBuBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jakbu_be/JakBuBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jakbu_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JakBuBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
